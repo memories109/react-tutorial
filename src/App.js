8b91cbf6e0746d4cffccec03c0d0a5c41f8528d2
Ghosts <memories109@gmail.com>
2021-12-01 21:38:28 +0900

    deep copy

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ import './App.css';
 
 function App() {
   let posts = '강남 맛집';
-  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','강남 술집','강남 노래방']);
-  
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','강남 술집']);
+  let [따봉, 따봉변경] = useState(0);
+  function 제목변경 (){
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
   return (
     <div className="App">
      <div className="black-nav">
         <div>개발 Blog</div>
      </div>
      
-     <div classname ="list">
-      <h3>{글제목[0]}</h3>
+     <button onClick={ 제목변경 }>버튼</button>
+
+     <div className ="list">
+      <h3>{글제목[0]} <span onClick={ ()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h3>
       <p>2월 17일 발행</p>
        <hr/>
      </div>
-     <div classname ="list">
+     <div className ="list">
       <h3>{글제목[1]}</h3>
       <p>2월 17일 발행</p>
        <hr/>
      </div>
-     <div classname ="list">
+     <div className ="list">
       <h3>{글제목[2]}</h3>
       <p>2월 17일 발행</p>
        <hr/>
      </div>
-     <div classname ="list">
-      <h3>{글제목[3]}</h3>
-      <p>2월 17일 발행</p>
-       <hr/>
-     </div>
+    
 
     </div>
   );
