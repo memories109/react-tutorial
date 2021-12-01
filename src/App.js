a90608ba777158f7126a2207efd525f1dbff89b4
Ghosts <memories109@gmail.com>
2021-12-01 20:59:09 +0900

    class app add

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+        <div>개발 Blog</div>
+     </div>
+     
+     <div classname ="list">
+      <h3>{posts}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+
+     </div>
     </div>
   );
 }
