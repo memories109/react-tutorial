fe2ba41b9855341a4944b5ec6c96617dbf279f79
Ghosts <memories109@gmail.com>
2021-12-01 21:14:33 +0900

    array

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 function App() {
   let posts = '강남 맛집';
-  let [글제목,글제목변경] = useState('남자 코트 추천');
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','강남 술집','강남 노래방']);
+  
   return (
     <div className="App">
      <div className="black-nav">
@@ -12,11 +13,26 @@ function App() {
      </div>
      
      <div classname ="list">
-      <h3>{글제목}</h3>
+      <h3>{글제목[0]}</h3>
       <p>2월 17일 발행</p>
        <hr/>
-
      </div>
+     <div classname ="list">
+      <h3>{글제목[1]}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+     </div>
+     <div classname ="list">
+      <h3>{글제목[2]}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+     </div>
+     <div classname ="list">
+      <h3>{글제목[3]}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+     </div>
+
     </div>
   );
 }
